173e59c85d0621388104f0c913c25ae7478b92cd
Chris Moore <103774436+Elusid108@users.noreply.github.com>
2026-09-13 10:38:23 -0400

    fixed start.bat

--- start.bat
+++ start.bat
@@ -1,6 +1,9 @@
 @echo off
 cd /d "%~dp0"
 
+rem Customer launch: do not inherit a DEBUG flag that would open DevTools.
+set DEBUG=
+
 if not exist "node_modules\" (
     echo Installing dependencies. This may take a minute...
     call npm install
@@ -10,7 +13,13 @@ if not exist "node_modules\" (
 call npm run build:ui
 if errorlevel 1 goto :fail
 
-start "" "%~dp0node_modules\electron\dist\electron.exe" "%~dp0"
+if not exist "node_modules\electron\dist\electron.exe" (
+    echo Electron is not installed. Run npm install and try again.
+    goto :fail
+)
+
+rem %~dp0 ends with a backslash; quoting it makes Electron see a stray " in the path.
+start "" "node_modules\electron\dist\electron.exe" .
 exit /b 0
 
 :fail
